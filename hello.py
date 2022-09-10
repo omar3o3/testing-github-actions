ddead00807086bba1bee814032267f13597acfd5
omar3o3 <otantawy53@gmail.com>
2022-09-10 18:22:44 -0400

    added doc string

--- hello.py
+++ hello.py
@@ -2,14 +2,15 @@
 AGE = 10
 NAME = "Omar"
 
+'''does stuff'''
 def age_in_months(input):
     output = input * 12
     print(output)
 
-
+'''does more stuff'''
 def print_greeting():
     print("hello world!")
 
-
+'''does even more stuff'''
 def print_goodbyes():
     print("goodbye world!")
