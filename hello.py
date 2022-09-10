aa2d7118b0532d500c54f716b0d2feab7e755151
omar3o3 <otantawy53@gmail.com>
2022-09-10 18:04:27 -0400

    test run

--- hello.py
+++ hello.py
@@ -1,13 +1,13 @@
 age = 10
 
-def age_in_months(input):
-    output = input * 12
-    print(output)
+# def age_in_months(input):
+#     output = input * 12
+#     print(output)
 
 
-def print_greeting():
-    print("hello world!")
+# def print_greeting():
+#     print("hello world!")
 
 
-def print_goodbyes():
-    print("goodbye world!")
+# def print_goodbyes():
+#     print("goodbye world!")
